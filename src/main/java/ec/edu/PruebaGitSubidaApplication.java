49516bfd813feda366d7709fd765e4f13a6126af
DennisJav <denis.javier.tapia.ortiz@gmail.com>
2022-01-10 20:27:03 -0500

    Subida Inicial

---
+++ src/main/java/ec/edu/PruebaGitSubidaApplication.java
@@ -0,0 +1,13 @@
+package ec.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaGitSubidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaGitSubidaApplication.class, args);
+	}
+
+}
